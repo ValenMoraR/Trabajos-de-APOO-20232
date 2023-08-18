6bfe958baebf9fc112dc6f42a2dcde2746794aa2
Valentina Morales <111003190+ValenMoraR@users.noreply.github.com>
2023-08-18 12:56:43 -0500

    Update E6.py

--- Tarea2/E6.py
+++ Tarea2/E6.py
@@ -1,1 +1,15 @@
+class Carta:
+    def __init__(self, valor, pinta):
+        self.valor = valor
+        self.pinta = pinta
 
+PINTA_CORAZON = "Corazón"
+PINTA_DIAMANTE = "Diamante"
+PINTA_TREBOL = "Trébol"
+PINTA_ESPADA = "Espada"
+
+carta1 = Carta(10, PINTA_CORAZON)
+carta2 = Carta(4, PINTA_TREBOL)
+
+print(f"Carta 1: Valor {carta1.valor}, Pinta {carta1.pinta}")
+print(f"Carta 2: Valor {carta2.valor}, Pinta {carta2.pinta}")
